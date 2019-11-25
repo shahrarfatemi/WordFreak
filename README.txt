916bf44ba32f42c6c909a3712fc9d896fdbd282f
Ubuntu <shahrarswapnil@gmail.com>
2019-11-25 10:52:06 -0500

    terminal and command related problems are solved now

--- README.txt
+++ README.txt
@@ -1,18 +1,27 @@
-In this assignment,the two dimensional character array 'words' is used to store all the distinct words in the input files.
-I have made an estimation of at most eight thousand(8000) distinct words so that we can test it with at most this number of words.
+In this assignment,the two dimensional character array 'words' is used to store all the
+distinct words of the input files.
+I have made an estimation of at most eight thousand(8000) distinct words so that we can
+test it with at most this number of words.
 Also, it was estimated that no word is longer than 100 characters.
 
-I used an integer array 'count' to store the occurences of a word.The i-th entry of that 'count' array is the frequency of the i-th entry of
+I used an integer array 'count' to store the occurences of a word.The i-th entry of that 
+'count' array is the frequency of the i-th entry of
 the 'words' array.This array was initialized with all '0' entries.
 
-At first I read the input file character by character and for any other symbol apart from the alphabet,I considered the previous scanned
-part to be a word and after that, I went through the 'words' array to check if a similar word existed.If a similar word was found,this
-current word was not added in the 'words' array,only the corresponding entry of the 'count' array was incremented.Otherwise,the word 
-was enlisted in the array and it's count was incremented.
-For all the input files, the files were read one by one in a loop.And the above task was done using the 'word_freak' function for each of
-them.So,at last I got the frequencies for each of the distinct words of those input files combined.
+At first I read the input file character by character and for any other symbol apart 
+from the alphabet,I considered the previous scanned part to be a word and after that, 
+I went through the 'words' array to check if a similar word existed.If a similar word
+was found,this current word was not added in the 'words' array,only the corresponding
+entry of the 'count' array was incremented.Otherwise,the word was enlisted in the array
+and it's count was incremented.
 
-Commands for running from linux terminal(makefile is attached in the folder)
-1)make
-2)./wordfreak <inputfiles' names with a space in between>
+For all the input files,the files were read one by one in a loop.And the above task 
+was done using the 'word_freak' function for each of them.So,at last I got the frequencies
+for each of the distinct words of those input files combined.
 
+A variable 'inputType' was used for the two methods of reading the inputs. 
+inputType = 1 => for taking file names as command line arguments
+inputType = 0 => for taking file names as standard inputs
+Also,environment variables were checked if an environment variable named 'WORD_FREAK'
+had been initialized so that the value of that could be used as a filename.
+So, thus all the three conditions for commands were fulfilled.
--- wordfreak.c
+++ wordfreak.c
@@ -1,48 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 #define MAX_WORDS  8000 //estimated maximum distinct words 8000
 #define MAX_LETTERS 100 //estimated maximum letters for a word 100
 
-char words[MAX_WORDS][MAX_LETTERS]; //words will be the array of distinct words
-char word[MAX_LETTERS];             //word will be used to the current word
+char **words;                       //words will be the array of distinct words
+char *word;                         //word will be used to the current word
 int  count[MAX_WORDS];              //for keeping a track of the frequency
 static int wordCount = 0;           //number of distinct words
-FILE *file;                         //file pointer to read from the text file
-
+int file;                           //file pointer to read from the text file
+int inputType = 0;                  //inputType defines the form of reading
+                                    //1 => command line args, 0 => std_input
 void word_freak(char * filePath){
     int i,found;
-    file = fopen(filePath, "r");    //try to open file
     
-    if (file == NULL){
-        printf("Unable to open file.\n");
+    if(inputType == 1){
+        file = open(filePath,O_RDONLY);  //try to open file
+    }
+    if (file == -1){                //when there is an errorin inputfile
+        char *errorStr = (char*)malloc(sizeof(char)*200);
+        sprintf(errorStr,"Unable to open file.\nerror : %s\n",strerror(0));
+        write(1,errorStr,strlen(errorStr));
+        free(errorStr);
+        return;
     }
     else{
     	int itr = 0,scanning = 0;   //itr is used for iterating through the current word
 				    //scanning variable is used to skip multiple spaces or other symbols among words
-    	char c;
-    	while((c = fgetc(file)) != EOF){ //scanning each word in character c
-            if(c >= 'A' && c <= 'Z'){
+        char *c = (char*)malloc(sizeof(char));
+        int type = (inputType==1)?file:0; //type is set to verify if the paths are given from
+                                          //command line or standard input
+    	while(read(type,c,1) != 0){ //scanning each word in character c
+            if(*c >= 'A' && *c <= 'Z'){
                 scanning = 1;
-                word[itr++] = (c+32);
+                word[itr++] = (*c+32);
             }
-            else if(c >= 'a' && c <= 'z'){
+            else if(*c >= 'a' && *c <= 'z'){
                 scanning = 1;
-                word[itr++] = c;
+                word[itr++] = *c;
             }
             else{
                 if(scanning){
                     found = 0;
             	    for (i=0; i < wordCount; i++){  //checking if the scanned word exits in the list of distinct words
-                        if (strcmp(words[i], word) == 0){
+                        if (strcmp(*(words+i), word) == 0){
                             found = 1;
                             break;
                         }
             	    }
             	    if (!found){
-                        strcpy(words[wordCount], word); //the word is a new one,that's why, adding this to the list
-	                count[wordCount++]++;           //incrementing the count of that word
+                        strcpy(*(words+wordCount), word); //the word is a new one,that's why, adding this to the list
+	                    count[wordCount++]++;             //incrementing the count of that word
                     }
        	            else{
             	        count[i]++;                     //incrementing the count of the matched word
@@ -57,26 +71,52 @@ void word_freak(char * filePath){
         } 
 
     }
-    fclose(file);   //closing the current text file                                      
+    if(inputType == 1){
+        close(file);   //closing the current text file
+    }
+                                          
 }
 
 
-int main(int argc, char * argv[])
+int main(int argc, char * argv[], char** envp)
 {
 
-
     int i;
+    char *str = (char *)malloc(sizeof(char)*100);  //this string is used for printing
+                                                  //using standard output
+    word = (char *)malloc(sizeof(char)*MAX_LETTERS);
+    words = (char **)malloc(sizeof(char)*MAX_WORDS);
+    for(i = 0 ; i < MAX_WORDS ; i++){             //for allocating spaces for 8000 words
+        *(words+i) = (char*)malloc(sizeof(char)*MAX_LETTERS);
+    }
     for (i=0; i<MAX_WORDS; i++)     //initializing all counts to zero
         count[i] = 0;
-
-    for(i = 1 ; i < argc ; i++){    //the input-file names are taken from terminal
-        printf("%s\n",argv[i]);
-        word_freak(argv[i]);
+    
+    if(argc <= 1){
+        if(getenv("WORD_FREAK") != NULL){     //if environment variable is used
+            inputType = 1;
+            //printf("path => %s\n",path);
+            word_freak(getenv("WORD_FREAK"));
+            //free(path);
+        }
+        else{                                     //if standard input is used
+            char d = 'a';
+            word_freak(&d);
+        }
     }
-
+    else{
+        inputType = 1;
+        for(i = 1 ; i < argc ; i++){    //the input-file names are taken from terminal
+            //printf("%s\n",argv[i]);
+            word_freak(argv[i]);
+        }   
+    }
     for (i = 0; i < wordCount; i++)
     {
-        printf("%-10s  => %d\n", words[i], count[i]);
+        sprintf(str,"%-10s  : %d\n", words[i], count[i]);
+        write(1,str,strlen(str));
     }
+    free(str);
+    free(word);
     return 0;
 }
